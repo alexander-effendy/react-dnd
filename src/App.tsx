936d9c9cf61a8d4fa8942d429fd32ba5b9afa6da
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-04-07 10:17:19 +1000

    dev: installed react vite ts, tailwindcss, shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+const App = () => {
+  return (
+    <div className="">
+      <Card>
+        <CardHeader>
+          <CardTitle>Item 1</CardTitle>
+        </CardHeader>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Item 1</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  )
+}
+
+export default App
