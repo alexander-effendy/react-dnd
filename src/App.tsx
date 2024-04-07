83d489e3feca9dac709b6eced534d809f744ccc8
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-04-07 10:33:00 +1000

    feat: simple drag and drop done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { Reorder } from 'framer-motion';
+
 import {
   Card,
   CardContent,
@@ -8,19 +11,28 @@ import {
 } from "@/components/ui/card"
 
 const App = () => {
-  return (
-    <div className="">
-      <Card>
-        <CardHeader>
-          <CardTitle>Item 1</CardTitle>
-        </CardHeader>
-      </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Item 1</CardTitle>
-        </CardHeader>
-      </Card>
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+  return (
+    <div className="bg-[#24cc87] h-screen w-screen overflow-y-auto">
+      <Reorder.Group values={items} onReorder={setItems}>
+      {items.map((item, index) => (
+        <Reorder.Item value={item} key={item}>
+        <Card key={index} className="m-8">
+          <CardHeader>
+            <CardTitle>Item {item}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Hi my name is Tiffany georgio and people just call me georgio.
+          </CardContent>
+        </Card>
+        </Reorder.Item>
+      ))}
+      </Reorder.Group>
+      
+
+      
     </div>
   )
 }
